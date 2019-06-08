95d5bc488920d30ad3f944e7c16f0ab33a66e547
Daenam Kim <daenam.kim@gmail.com>
2019-06-08 17:56:07 +0900

    Add trasanction event

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <form @submit.prevent="onSubmit">
-      <input type="text" v-model="money" />
-      <input type="submit" />
-    </form>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <div>
+      <input type="text" v-model="ether" v-bind:disabled="isPlaying"/>
+    </div>
+    <div>
+      <button @click="onPlay" v-bind:disabled="isPlaying">Play!</button>
+    </div>
+    <div>
+      <a v-bind:href="etherScan + txHash" target="_blank">{{ this.txHash }}</a>
+    </div>
   </div>
 </template>
 
@@ -19,41 +26,54 @@ export default {
   components: {},
   data() {
     return {
-      money: '',
+      ether: '0.1',
+      etherScan: 'https://ropsten.etherscan.io/tx/',
+      txHash: '',
+      isPlaying: false,
     };
   },
   created() {
     console.log(web3.eth.getAccounts());
+
+    cashapon.events.Played({fromBlock: "latest"}, (error, event) => {
+          console.log(event);
+          this.isPlaying = false;
+          this.txHash = event.transactionHash;
+        }).on('data', (event) => {
+          console.log('data', event); // same results as the optional callback above
+        }).on('changed', (event) => {
+            // remove event from local database
+            console.log('changed', event); // same results as the optional callback above
+        }).on('error', (error) => {
+          console.error(error);
+          this.isPlaying = false;
+          this.txHash = '';
+        });
   },
   methods: {
-    async onSubmit() {
+    async onPlay() {
+      if (parseFloat(this.ether) < 0.1 || parseFloat(this.ether) > 1 || this.isRunning) {
+        return;
+      }
+
+      this.txHash = '';
+      this.isPlaying = true;
+
       try {
         const accounts = await web3.eth.getAccounts();
-        // For Test
-        // await web3.eth.sendTransaction({
-        //     from: accounts[0],
-        //     to: '0x3F37278403BF4Fa7c2B8fa0D21Af353c554641A1',
-        //     value: web3.utils.toWei('1'),
-        //     gas: 2000000
-        // });
-        if (!this.money) {
-          return;
-        }
-
         await cashapon.methods.play().send({
           from: accounts[0],
-          value: web3.utils.toWei(this.money, 'ether'),
+          value: web3.utils.toWei(this.ether, 'ether'),
         });
-
-        // TODO: get result
       } catch (error) {
         console.log(error);
+        this.isPlaying = false;
       }
     }
   },
   watch: {
-    money(newMoney) {
-      console.log(newMoney);
+    ether(newEther) {
+      console.log(newEther);
     }
   }
 }

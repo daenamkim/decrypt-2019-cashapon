aac842715b5f2a95cd0747efe4168aad7f87b06e
Daenam Kim <daenam.kim@gmail.com>
2019-06-09 06:50:38 +0900

    Add gasha animation and refactor codes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 
 <template>
   <v-app>
-    <v-toolbar>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>$$ Cashapon $$</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+    <v-toolbar class="toolbar-background"></v-toolbar>
     <v-container fluid>
       <v-card>
         <v-layout align-center justify-center column fill-height>
           <v-flex xs12 class="relative">
-            <img alt="Cashapon" src="./assets/gatcha.png">
+            <img alt="Cashapon" src="./assets/gasha-machine.png">
             <img
-              v-bind:class="['gatcha-handle', isPlaying ? 'gatcha-handle-spin' : '']"
-              src="./assets/gatcha-handle.png"
+              v-bind:class="['gasha-handle', isPlaying ? 'gasha-handle-spin' : '']"
+              src="./assets/gasha-handle.png"
               @click="onPlay"
             >
           </v-flex>
@@ -33,25 +29,36 @@
           </v-flex>
         </v-layout>
       </v-card>
+      <v-dialog v-model="dialogVisible" width="500">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>Yay! Gasha!!</v-card-title>
+          <v-card-text class="test">
+            <v-layout align-center justify-center column fill-height>
+              <v-flex xs12>
+                <div style="position: relative; height: 250px; width: 216px; margin: 0 auto;">
+                  <img
+                    v-bind:class="['test-img', !GashaToggle ? 'test-img-transparent' : '']"
+                    src="./assets/gasha-close.png"
+                  >
+                  <img
+                    v-bind:class="['test-img', GashaToggle ? 'test-img-transparent' : '']"
+                    src="./assets/gasha-open.png"
+                  >
+                </div>
+              </v-flex>
+              <v-flex xs12>
+                <v-btn round color="success" @click="onGoToEtherscan">See what I cashaed</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click="dialogVisible = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
-
-    <v-dialog v-model="dialogVisible" width="500">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>Yay! Gatcha!!</v-card-title>
-        <v-card-text>
-          You can check your coin of the ether you used below
-          <a
-            v-bind:href="etherScan + txHash"
-            target="_blank"
-          >{{ this.txHash }}</a>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" flat @click="dialogVisible = false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
     <v-footer class="pa-3">
       <v-spacer></v-spacer>
       <div>Anton Buenavista, Daenam Kim, Masahiro Takeda and Takuya Umeki @ Decrypt Tokyo 2019</div>
@@ -63,6 +70,7 @@
 /* eslint-disable no-console */
 import web3 from './web3';
 import cashapon from './cashapon';
+import { clearInterval, setInterval } from 'timers';
 
 export default {
   name: 'app',
@@ -74,10 +82,13 @@ export default {
       txHash: '',
       isPlaying: false,
       dialogVisible: false,
+      numTokens: 1,
+      GashaToggleTimerId: null,
+      GashaToggle: false,
     };
   },
   created() {
-    console.log(web3.eth.getAccounts());
+    console.log('ACCOUNT:', web3.eth.getAccounts());
 
     cashapon.events
       .Played({ fromBlock: 'latest' }, (error, event) => {
@@ -100,6 +111,9 @@ export default {
       });
   },
   methods: {
+    onGoToEtherscan() {
+      window.open(this.etherScan + this.txHash, '_blank');
+    },
     async onPlay() {
       if (
         parseFloat(this.ether) < 0.1 ||
@@ -114,10 +128,20 @@ export default {
 
       try {
         const accounts = await web3.eth.getAccounts();
-        await cashapon.methods.play().send({
-          from: accounts[0],
-          value: web3.utils.toWei(this.ether, 'ether'),
-        });
+        if (this.numTokens > 1) {
+          await cashapon.methods.play().send({
+            gas: '500000',
+            from: accounts[0],
+            value: web3.utils.toWei(this.ether, 'ether'),
+          });
+        } else {
+          // TODO: add playMultiple(numTokens)
+          await cashapon.methods.playMultiple(this.numTokens).send({
+            gas: '1100000',
+            from: accounts[0],
+            value: web3.utils.toWei(this.ether, 'ether'),
+          });
+        }
       } catch (error) {
         console.log(error);
         this.isPlaying = false;
@@ -128,11 +152,37 @@ export default {
     ether(newEther) {
       console.log(newEther);
     },
+    dialogVisible(newDialogVisible) {
+      if (newDialogVisible) {
+        this.GashaToggleTimerId = setInterval(() => {
+          this.GashaToggle = !this.GashaToggle;
+        }, 500);
+      } else {
+        clearInterval(this.GashaToggleTimerId);
+      }
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.test-img {
+  transition: opacity 0s ease-in-out;
+  position: absolute;
+}
+
+.test-img-transparent {
+  opacity: 0;
+}
+
+.toolbar-background {
+  background: no-repeat url('./assets/logo.png'),
+    linear-gradient(to right, #6cd4b3 0%, #6cd4b3 50%, #fbe357 50%, #fbe357 50%);
+  height: 150px;
+  background-size: contain;
+  background-position: center center;
+}
+
 .button-width {
   width: 195px !important;
 }
@@ -141,13 +191,13 @@ export default {
   position: relative;
 }
 
-.gatcha-handle {
+.gasha-handle {
   z-index: 10;
   position: absolute;
   top: 177px;
-  left: 267px;
+  left: 155px;
 }
-.gatcha-handle-spin {
+.gasha-handle-spin {
   animation: element-spin 2s linear infinite;
 }
 
@@ -158,5 +208,14 @@ export default {
   100% {
     transform: rotate(360deg);
   }
+}
+
+.logo {
+  object-fit: cover;
+  height: 50px;
+}
+
+.no-text-decoration {
+  text-decoration: none;
 }
 </style>
